5034d4ee18e3f1b0e9fa5ab117e8bd11a2ac4ab4
Janiece-Lewis <jlewilewi@gmail.com>
2023-05-18 13:39:14 -0400

    added delete route

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const router = require('express').Router();
 const fs = require('fs');
-const uniqid = require('uniqid'); 
+const uniqid = require('uniqid');
 
 // middleware
 router.get('/notes', (req, res) => {
@@ -18,24 +18,39 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
     console.log(req.body)
-    req.body.id =uniqid()
+    req.body.id = uniqid()
     console.log(req.body)
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
-     
+
         let notes = JSON.parse(data)
         notes.push(req.body)
         console.log(notes)
 
-        fs.writeFile('./db/db.json', JSON.stringify(notes,null,`\n`), (err) => {
+        fs.writeFile('./db/db.json', JSON.stringify(notes, null, `\n`), (err) => {
             if (err) throw err;
             console.log('The file has been saved!');
             return res.status(200).json(notes)
-          }); 
-      
+        });
+
     });
 });
+router.delete('/notes/:id', (req, res) => {
+    console.log(req.params.id)
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
 
+        let notes = JSON.parse(data)
+        let updatedNotes = notes.filter(note => note.id !== req.params.id)
+        console.log(updatedNotes)
+        fs.writeFile('./db/db.json', JSON.stringify(updatedNotes, null, `\n`), (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+            return res.status(200).json(updatedNotes)
+        });
+
+    });
+})
 
 
 
